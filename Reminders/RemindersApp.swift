214eee1024bc2a456413f5c6a7f0fd6ed794be94
KimKyungHwan <u4rang@gmail.com>
2022-11-30 22:46:59 +0900

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by KYUNG HWAN KIM on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
